617b47afb1ec6d2e481ac31d78e155257e1deac8
Anees Muhammed <anees.muhammed@codeprism.in>
2025-03-13 15:47:30 +0530

    error fix

--- trackers/person_and_phone.py
+++ trackers/person_and_phone.py
@@ -342,6 +342,7 @@ def detect_phone_and_person(video_path, res_dict):
 
     phone_event = {"event_no": 0}
     person_event = {"event_no": 0, "curr": ""}
+    frames_captured = None
 
     try:
         start_time = time.time()
